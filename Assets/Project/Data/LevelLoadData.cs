5ab9273a13fb1b58d1932ffc860434c8796eb1a6
Qma <mk348437@gmail.com>
2023-03-02 21:52:35 +0100

    wip commit

---
+++ Assets/Project/Data/LevelLoadData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Meta;
+
+namespace Project.Data
+{
+    [CreateAssetMenu(menuName = AssetMenuPaths.Data.Level.LoadData)]
+    public class LevelLoadData : ScriptableObject
+    {
+
+    }
+    [CreateAssetMenu(menuName = AssetMenuPaths.Data.Level.MetaData)]
+    public class LevelMetaData : ScriptableObject
+    {
+
+    }
+    public class LevelProgressData
+    {
+
+    }
+}
